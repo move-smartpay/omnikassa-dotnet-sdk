2e2cc042bcf5491654200fc38c07054ae76819a6
Przemek Grupa <przemek.grupa@rabobank.nl>
2025-09-05 15:48:23 +0200

    - Some small fixes.

--- src/OmniKassa/Model/Response/CardOnFile.cs
+++ src/OmniKassa/Model/Response/CardOnFile.cs
@@ -1,9 +1,5 @@
 using Newtonsoft.Json;
-using System;
 using OmniKassa.Model.Enums;
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("OmniKassa.Tests")]
 
 namespace OmniKassa.Model.Response
 {
@@ -16,37 +12,37 @@ namespace OmniKassa.Model.Response
         /// Uniquely identifies the card on file
         /// </summary>
         [JsonProperty("id")]
-        public string Id { get; internal set; }
+        public string Id { get; set; }
 
         /// <summary>
         /// The last 4 digits of the card number
         /// </summary>
         [JsonProperty("last4Digits")]
-        public string Last4Digits { get; internal set; }
+        public string Last4Digits { get; set; }
 
         /// <summary>
         /// The brand of the card
         /// </summary>
         [JsonProperty("brand")]
-        public string Brand { get; internal set; }
+        public string Brand { get; set; }
 
         /// <summary>
         /// The expiry of the card in YYYY-MM format
         /// </summary>
         [JsonProperty("cardExpiry")]
-        public string CardExpiry { get; internal set; }
+        public string CardExpiry { get; set; }
 
         /// <summary>
         /// The expiry of the token in YYYY-MM format
         /// </summary>
         [JsonProperty("tokenExpiry")]
-        public string TokenExpiry { get; internal set; }
+        public string TokenExpiry { get; set; }
 
         /// <summary>
         /// The status of the token
         /// </summary>
         [JsonProperty("status")]
         [JsonConverter(typeof(EnumJsonConverter<CardStatus>))]
-        public CardStatus? Status { get; internal set; }
+        public CardStatus? Status { get; set; }
     }
 }
